0112bad2b5688297474b3d55a59b82fb1c02c743
Robert García Morel <robertgarcia95@gmail.com>
2022-09-09 09:00:50 +0200

    Added test of LazyImgDirective

--- projects/rob-lib/src/lib/directives/lazy-img/lazy-img.directive.spec.ts
+++ projects/rob-lib/src/lib/directives/lazy-img/lazy-img.directive.spec.ts
@@ -1,8 +1,41 @@
 import { LazyImgDirective } from './lazy-img.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { getFixtureNativeElement } from '@robLib/helpers/spec-helpers';
 
-describe('LazyImgDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LazyImgDirective();
+@Component({
+  template: '<img src="" alt="characterDetail.img_alt" />',
+})
+class MockComponent {}
+
+describe('GIVEN LazyImgDirective', () => {
+  let directive: LazyImgDirective;
+  let fixture: ComponentFixture<MockComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MockComponent],
+      imports: [LazyImgDirective],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MockComponent);
+    fixture.detectChanges();
+    directive = new LazyImgDirective(fixture);
+  });
+
+  it('SHOULD create an instance', () => {
     expect(directive).toBeTruthy();
+  });
+
+  describe('ONCE initialized', () => {
+    let fixtureNativeElement: HTMLImageElement;
+
+    beforeEach(() => {
+      fixtureNativeElement = getFixtureNativeElement(fixture);
+    });
+
+    it('SHOULD be checked attribute lazy', () => {
+      expect(fixtureNativeElement.getAttribute('loading')).toEqual('lazy');
+    });
   });
 });
