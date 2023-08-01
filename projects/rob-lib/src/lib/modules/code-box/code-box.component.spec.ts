8508627606d2a34329a9631e6fd3e4359b0d2187
Robert <robertgarcia95@gmail.com>
2023-08-01 19:49:31 +0200

    Lint

--- projects/rob-lib/src/lib/modules/code-box/code-box.component.spec.ts
+++ projects/rob-lib/src/lib/modules/code-box/code-box.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CodeBoxComponent } from './code-box.component';
 
 describe('CodeBoxComponent', () => {
@@ -7,10 +6,7 @@ describe('CodeBoxComponent', () => {
   let fixture: ComponentFixture<CodeBoxComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ CodeBoxComponent ]
-    })
-    .compileComponents();
+    await TestBed.configureTestingModule({ imports: [CodeBoxComponent] }).compileComponents();
 
     fixture = TestBed.createComponent(CodeBoxComponent);
     component = fixture.componentInstance;
