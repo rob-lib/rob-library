8f40e179e33a1aa70ee4f381a4aef4942521385e
Robert <robertgarcia95@gmail.com>
2023-10-13 11:18:19 +0200

    Fix CodeBoxComponent by not copy string

--- projects/rob-lib/src/lib/modules/code-box/code-box.component.ts
+++ projects/rob-lib/src/lib/modules/code-box/code-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, Input } from '@angular/core';
+import { Component, ElementRef, ViewChild, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { Clipboard, ClipboardModule } from '@angular/cdk/clipboard';
@@ -14,33 +14,41 @@ import { Expand } from '../../animations';
     MAT_TOOLTIP_SCROLL_STRATEGY_FACTORY_PROVIDER,
     { provide: HAMMER_GESTURE_CONFIG, useClass: HammerGestureConfig },
   ],
-  templateUrl: './code-box.component.html',
+  template: `
+    <div class="code-box__container" #codeBox>
+      <ng-content></ng-content>
+      <mat-icon
+        [@click]="clickAction"
+        (click)="copyClipboard()"
+        #tooltip="matTooltip"
+        [matTooltip]="tooltipMessage"
+        disableTooltipInteractivity="true">
+        content_paste
+      </mat-icon>
+    </div>
+  `,
   styleUrls: ['./code-box.component.scss'],
   animations: [Expand('click')],
 })
-export class CodeBoxComponent implements AfterViewInit {
-  @ViewChild('codeBox') codeBox!: ElementRef<any>;
-  @ViewChild('tooltip') tooltip!: MatTooltip;
+export class CodeBoxComponent {
+  @ViewChild('codeBox', { static: true }) codeBox!: ElementRef<any>;
+  @ViewChild('tooltip', { static: true }) tooltip!: MatTooltip;
   @Input() tooltipMessage = 'Copy on clipboard';
 
-  clipboardData!: string;
   clickAction = false;
   showTooltip = false;
 
   constructor(private clipboard: Clipboard) {}
 
-  ngAfterViewInit(): void {
-    this.clipboardData = (this.codeBox.nativeElement.childNodes[1].data as string).trim();
-  }
-
   copyClipboard(): void {
     this.clickAction = true;
     this.tooltip.disabled = false;
     this.tooltip.show();
-    this.clipboard.copy(this.clipboardData);
+    const [{ textContent }] = this.codeBox.nativeElement.childNodes;
+    this.clipboard.copy(textContent);
 
     setTimeout(() => {
       this.clickAction = false;
-    }, 5000);
+    }, 500);
   }
 }
